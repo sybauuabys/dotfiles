699d70cdbd7de0de5cea10acd9e8e419ff0d0b9c
Sybauuabys <shresthasujan.me@gmail.com>
2025-09-08 11:29:03 +0545

    Catppucin Theme

--- .config/nvim/lua/josean/plugins/colorscheme.lua
+++ .config/nvim/lua/josean/plugins/colorscheme.lua
@@ -1,45 +1,48 @@
+-- ~/.config/nvim/lua/plugins/colorscheme.lua
 return {
-  "folke/tokyonight.nvim",
-  priority = 1000,
-  config = function()
-    local transparent = false -- set to true if you would like to enable transparency
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      local transparent = false -- set to true if you want transparency
 
-    local bg = "#011628"
-    local bg_dark = "#011423"
-    local bg_highlight = "#143652"
-    local bg_search = "#0A64AC"
-    local bg_visual = "#275378"
-    local fg = "#CBE0F0"
-    local fg_dark = "#B4D0E9"
-    local fg_gutter = "#627E97"
-    local border = "#547998"
+      -- Custom background & palette tweaks
+      local bg = "#0f0f23"
+      local bg_dark = "#0a0a1a"
+      local bg_high = "#2a2a3e"
+      local bg_visual = "#45475a"
+      local fg = "#cdd6f4"
+      local fg_dark = "#bac2de"
+      local fg_gutter = "#6c7086"
+      local border = "#7f849c"
+      local blue = "#89b4fa"
 
-    require("tokyonight").setup({
-      style = "night",
-      transparent = transparent,
-      styles = {
-        sidebars = transparent and "transparent" or "dark",
-        floats = transparent and "transparent" or "dark",
-      },
-      on_colors = function(colors)
-        colors.bg = bg
-        colors.bg_dark = transparent and colors.none or bg_dark
-        colors.bg_float = transparent and colors.none or bg_dark
-        colors.bg_highlight = bg_highlight
-        colors.bg_popup = bg_dark
-        colors.bg_search = bg_search
-        colors.bg_sidebar = transparent and colors.none or bg_dark
-        colors.bg_statusline = transparent and colors.none or bg_dark
-        colors.bg_visual = bg_visual
-        colors.border = border
-        colors.fg = fg
-        colors.fg_dark = fg_dark
-        colors.fg_float = fg
-        colors.fg_gutter = fg_gutter
-        colors.fg_sidebar = fg_dark
-      end,
-    })
+      require("catppuccin").setup({
+        flavour = "frappe", -- latte, frappe, macchiato, mocha
+        transparent_background = transparent,
+        term_colors = true,
 
-    vim.cmd("colorscheme tokyonight")
-  end,
+        color_overrides = {
+          mocha = {
+            base = transparent and "NONE" or bg,
+            mantle = transparent and "NONE" or bg_dark,
+            crust = transparent and "NONE" or bg_dark,
+            surface0 = bg_high,
+            surface1 = bg_visual,
+            surface2 = border,
+            overlay0 = fg_gutter,
+            overlay1 = fg_dark,
+            overlay2 = border,
+            text = fg,
+            subtext0 = fg_gutter,
+            subtext1 = fg_dark,
+            blue = blue,
+          },
+        },
+      })
+
+      vim.cmd.colorscheme("catppuccin")
+    end,
+  },
 }
